d8f200fd02b5150c776c440588ce494b90e8a601
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-14 13:11:05 +0530

    Created Save coupon method in COupon DAO

--- src/com/pk/cnp/dao/CouponDAO.java
+++ src/com/pk/cnp/dao/CouponDAO.java
@@ -1,5 +1,24 @@
 package com.pk.cnp.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.pk.cnp.model.Coupon;
+import com.pk.cnp.util.ConnectionUtil;
+
 public class CouponDAO {
-	
+	public void save(Coupon coupon) {
+		Connection con = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement ps = con.prepareStatement("insert into coupon "
+					+ "(code,discount,exp_date) values(?,?,?)");
+			ps.setString(1, coupon.getCode());
+			ps.setBigDecimal(2, coupon.getDiscount());
+			ps.setString(3, coupon.getExp());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
