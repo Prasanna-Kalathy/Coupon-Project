52b528551288802f3c5874e7a18fb1d9c93aaf2c
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-14 13:31:41 +0530

    Created find coupon method in Coupon DAO

--- src/com/pk/cnp/dao/CouponDAO.java
+++ src/com/pk/cnp/dao/CouponDAO.java
@@ -2,6 +2,7 @@ package com.pk.cnp.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.pk.cnp.model.Coupon;
@@ -11,12 +12,30 @@ public class CouponDAO {
 	public void save(Coupon coupon) {
 		Connection con = ConnectionUtil.getConnection();
 		try {
-			PreparedStatement ps = con.prepareStatement("insert into coupon "
-					+ "(code,discount,exp_date) values(?,?,?)");
+			PreparedStatement ps = con
+					.prepareStatement("insert into coupon " + "(code,discount,exp_date) values(?,?,?)");
 			ps.setString(1, coupon.getCode());
 			ps.setBigDecimal(2, coupon.getDiscount());
 			ps.setString(3, coupon.getExp());
 			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void findcode(String code) {
+		Coupon coupon = new Coupon();
+		Connection con = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement ps = con.prepareStatement("select * from coupon where code=?");
+			ps.setString(1, code);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				coupon.setId(rs.getInt(1));
+				coupon.setCode(rs.getString(2));
+				coupon.setDiscount(rs.getBigDecimal(3));
+				coupon.setExp(rs.getString(4));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
